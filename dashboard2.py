279707836e3ff1d8d52f852018ae7e94797e7667
Henry Shi <henry.shi.northvan@gmail.com>
2025-12-04 01:41:41 -0800

    Test using markdown directly instead of custom function

--- dashboard2.py
+++ dashboard2.py
@@ -510,9 +510,9 @@ def show_dashboard():
         #custom_subheader(f"Time Series: Word Frequency Over Time",help_text = f"See how often your input term, along with all translations and synonyms in other languages, is used over time in this interactive smoothed time series graph (5-year moving average to reduce noise). Hover over lines, show/hide entries by clicking in the legend, use the language filter dropdown, or drag the range slider at the bottom to select time frame.")
         
         st.markdown(
-        "<div class='custom-subheader'>Time Series: Word Frequency Over Time</div>",
+        "Time Series: Word Frequency Over Time",
         unsafe_allow_html=True,
-        help= f"See how often your input term, along with all translations and synonyms in other languages, is used over time in this interactive smoothed time series graph (5-year moving average to reduce noise). Hover over lines, show/hide entries by clicking in the legend, use the language filter dropdown, or drag the range slider at the bottom to select time frame."
+        help= "See how often your input term, along with all translations and synonyms in other languages, is used over time in this interactive smoothed time series graph (5-year moving average to reduce noise). Hover over lines, show/hide entries by clicking in the legend, use the language filter dropdown, or drag the range slider at the bottom to select time frame."
         )
 
         time_chart = create_timeseries(st.session_state['final_data'])
