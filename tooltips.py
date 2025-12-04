6e8b3d3087737eb49c7d73d86d7d680286e4492c
Henry Shi <henry.shi.northvan@gmail.com>
2025-12-04 01:26:35 -0800

    Test centering + tooltip again

--- tooltips.py
+++ tooltips.py
@@ -1,7 +1,7 @@
-world_map_tooltip = """Explore how your search term's usage varies across countries and languages in this interactive map via color intensity. Hover over each country to see specifics like primary language, and  move the date slider to compare trends over time. Note that the map is based on primary languages spoken by each country in 2015."""
+world_map_tooltip = "Explore how your search term's usage varies across countries and languages in this interactive map via color intensity. Hover over each country to see specifics like primary language, and  move the date slider to compare trends over time. Note that the map is based on primary languages spoken by each country in 2015."
 
-bar_graph_tooltip = """Compare how much a concept (search term + synonyms) is written about in each language. Hover over each block to see the exact frequencies for that word/phrase. """
+bar_graph_tooltip = "Compare how much a concept (search term + synonyms) is written about in each language. Hover over each block to see the exact frequencies for that word/phrase."
 
-word_cloud_tooltip = """Observe the popularity of each translation or synonym in different languages. The larger the word or phrase, the more frequently it is used, while the color corresponds to what language it's from (see bar graph colors). """
+word_cloud_tooltip = "Observe the popularity of each translation or synonym in different languages. The larger the word or phrase, the more frequently it is used, while the color corresponds to what language it's from (see bar graph colors)."
 
-timeseries_tooltip = """See how often your input term, along with all translations and synonyms in other languages, is used over time in this interactive smoothed time series graph (5-year moving average to reduce noise). Hover over lines, show/hide entries by clicking in the legend, use the language filter dropdown, or drag the range slider at the bottom to select time frame."""
+timeseries_tooltip = "See how often your input term, along with all translations and synonyms in other languages, is used over time in this interactive smoothed time series graph (5-year moving average to reduce noise). Hover over lines, show/hide entries by clicking in the legend, use the language filter dropdown, or drag the range slider at the bottom to select time frame."
